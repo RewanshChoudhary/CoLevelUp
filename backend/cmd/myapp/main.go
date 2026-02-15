a983275a587027cb83eaff8f5ce928cbc0147360
RewanshChoudhary <rewanshchoudhary57@gmail.com>
2026-02-15 17:43:33 +0530

    starting

--- backend/cmd/myapp/main.go
+++ backend/cmd/myapp/main.go
@@ -1,5 +1,22 @@
+package main
 
+import (
+	"net/http"
 
-func main(){
-	fmt.Println("")
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.CORS())
+
+	e.GET("/", func(c *echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	if err := e.Start(":1323"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
 }
